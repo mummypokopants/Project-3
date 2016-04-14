ab9805f0626a370b36288d5c327976c8676a20b3
David Tambunan <dkat90@gmail.com>
2016-04-14 14:33:32 +0800

    modified documentation partial

--- app.js
+++ app.js
@@ -12,10 +12,7 @@ var app = express()
 
 app.use(methodOverride('_method'))
 
-mongoose.connect('mongodb://localhost:27017/carparks')
-
-
-var routes = require('./config/routes');
+var routes = require('./routes/index');
 
 
 app.use(logger('dev'));
