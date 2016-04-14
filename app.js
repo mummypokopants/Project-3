3f986d113071ca29bffbbc7dbde9ef2767ef24ce
David Tambunan <dkat90@gmail.com>
2016-04-14 23:42:02 +0800

    added mongoUri database and variable

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var connect             = require('connect')
 var methodOverride      = require('method-override')
 var config              = require('./config/config')
 var app                 = express()
+var mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost/carparks'
+mongoose.connect(mongoUri);
 
 app.use(methodOverride('_method'))
 
@@ -31,7 +33,7 @@ var mongoose = require('mongoose');
 var db = mongoose.connection;
 
 // connect to the db
-mongoose.connect(config.db);
+// mongoose.connect(config.db);
 
 
 app.all('/*', function(req, res, next) {
